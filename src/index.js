efec9f05cdea06d17d884e27a01a54c1ba83a3b0
James Lake <jh.lake@uniandes.edu.co>
2019-04-02 09:10:33 -0500

    Ejericio en clase

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import {IntlProvider, addLocaleData} from 'react-intl';
+import esLocaleData from 'react-intl/locale-data/es';
+import localeEnMessages from './locales/en';
+import localeEsMessages from "./locales/es";
 import JobsList from "./components/jobsList";
 
-ReactDOM.render(<JobsList/>, document.getElementById("root"));
+addLocaleData(esLocaleData);
+
+
+
+function langSelector(){
+   if (window.navigator.language==="en-US") {
+        return (localeEnMessages);
+   }else{
+        return localeEsMessages;
+   }
+
+}
+
+ReactDOM.render(
+	<IntlProvider locale={window.navigator.language} messages= {langSelector()}>
+		<JobsList/>
+	</IntlProvider>, document.getElementById("root")
+);
